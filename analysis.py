26373a5dd9230985ed49a3368ace3133d4f6e98c
nielsbongers <nielsbongers5@gmail.com>
2024-03-04 09:29:29 +0100

    Comparing latest hash files

--- analysis.py
+++ analysis.py
@@ -1,35 +1,38 @@
+import re
+from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
 
-# file_path_list = [
-#     Path("file_hashes/18102023 - 048cf1f6-f1ab-46a6-8c94-9e461e05da49.csv"),
-#     Path("file_hashes/18102023 - 4f09f645-8162-45ca-880d-98379018d6eb.csv"),
-# ]
+file_hash_lists = Path("file_hashes").glob("**/*.csv")
 
-# for file_path in file_path_list:
-#     with open(file_path, "r", encoding="utf8") as f:
-#         with open(
-#             "file_hashes/" + file_path.stem + " - corrected" + file_path.suffix,
-#             "w",
-#             encoding="utf8",
-#         ) as g:
-#             for line in f:
-#                 if len(line) < 33:
-#                     g.write(line)
-#                     continue
+file_data = []
 
-#                 file_path = line[0:-34]
-#                 uuid = line[-33:].strip()
-#                 corrected_csv_line = f'"{file_path}","{uuid}"'
-#                 g.write(corrected_csv_line)
-#                 g.write("\n")
+for file_hash_list in file_hash_lists:
+    file_name = file_hash_list.stem
 
+    date_string = re.search("([0-9]+)", file_name)
 
-df1 = pd.read_csv("file_hashes/13112023 - 2215dd83-9053-4984-915c-3a4c5d5bb1ec.csv")
-df2 = pd.read_csv("file_hashes/13112023 - 29623e69-23fa-45eb-a925-b236d3f2c7d6.csv")
+    if date_string is None:
+        continue
 
-df = pd.concat([df1, df2])
+    date_string = date_string.group()
+    date = datetime.strptime(date_string, "%d%m%Y")
+    entry_data = {"file_path": file_hash_list, "file_name": file_name, "date": date}
+    file_data.append(entry_data)
+
+df = pd.DataFrame(file_data)
+df = df.sort_values("date", ascending=False)
+
+latest_date = df["date"].max()
+latest_files = df[df["date"] == latest_date]
+
+data_frame_list = []
+for file_path in latest_files["file_path"]:
+    data_frame_list.append(pd.read_csv(file_path))
+
+df = pd.concat(data_frame_list)
+
 df["duplicated"] = df["hash"].duplicated(keep=False)
 df = df[df["duplicated"] == False]
 
